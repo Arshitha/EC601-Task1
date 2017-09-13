ea77de8da7de245c262b222397bb6d3e218ae3f7
Arshitha <arsh2794@gmail.com>
2017-09-13 16:37:15 -0400

    Update cascadedetect.cpp
    cautionary warning comment

--- modules/objdetect/src/cascadedetect.cpp
+++ modules/objdetect/src/cascadedetect.cpp
@@ -1,6 +1,9 @@
 /*M///////////////////////////////////////////////////////////////////////////////////////
-//
-//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//            
+//         **** THIS CODE DOESN'T COMPILE. USED THE FILE JUST TO UNDERSTAND GITHUB. ****
+// 
+
+//IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
 //
 //  By downloading, copying, installing or using the software you agree to this license.
 //  If you do not agree to this license, do not download, install,
