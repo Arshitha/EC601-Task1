f1bb3e42a5037f12c52dd26ca017fedfd610c3cb
Arshitha <arsh2794@gmail.com>
2017-09-13 18:16:02 -0400

    Update main.cpp
    cautionary warning

--- modules/objdetect/src/main.cpp
+++ modules/objdetect/src/main.cpp
@@ -1,7 +1,8 @@
 /*M///////////////////////////////////////////////////////////////////////////////////////
-//
+// ***** WARNING: THIS CODE WILL NOT COMPILE *****
 //  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
-//
+// 
+//  
 //  By downloading, copying, installing or using the software you agree to this license.
 //  If you do not agree to this license, do not download, install,
 //  copy or use the software.
